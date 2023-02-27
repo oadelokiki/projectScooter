13dd7c1db74a3d92323b1819b99a877cadde86b5
oadelokiki <olus.software@gmail.com>
2023-02-27 00:11:36 -0500

    with jest tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,77 +1,60 @@
-const assert = require('assert');
-
-describe('ScooterApp', function() {
-
-    let scooterApp;
-
-    beforeEach(function() {
-        scooterApp = new ScooterApp();
+describe("Scooter", () => {
+  describe("rent", () => {
+    it("throws an error if scooter needs to charge", () => {
+      const scooter = new Scooter();
+      scooter.charge = 10;
+      expect(() => {
+        scooter.rent(new User());
+      }).toThrow("scooter needs to charge");
     });
 
-    describe('#registerUser', function() {
-        it('should register a new user', function() {
-            scooterApp.registerUser("John", "password123", 25);
-            assert.deepStrictEqual(Object.keys(scooterApp.registeredUsers), ["John"]);
-        });
-        it('should throw an error for underage users', function() {
-            assert.throws(() => scooterApp.registerUser("Alice", "password123", 15), Error("too young to register"));
-        });
-        it('should throw an error if user is already registered', function() {
-            scooterApp.registerUser("John", "password123", 25);
-            assert.throws(() => scooterApp.registerUser("John", "password456", 30), Error("already registered"));
-        });
+    it("throws an error if scooter needs repair", () => {
+      const scooter = new Scooter();
+      scooter.isBroken = true;
+      expect(() => {
+        scooter.rent(new User());
+      }).toThrow("scooter needs repair");
     });
 
-    describe('#loginUser', function() {
-        it('should log in an existing user with correct password', function() {
-            scooterApp.registerUser("John", "password123", 25);
-            scooterApp.loginUser("John", "password123");
-            assert.strictEqual(scooterApp.registeredUsers["John"].loggedIn, true);
-        });
-        it('should throw an error for non-existing user', function() {
-            assert.throws(() => scooterApp.loginUser("Alice", "password123"), Error("Username or password incorrect"));
-        });
-        it('should throw an error for incorrect password', function() {
-            scooterApp.registerUser("John", "password123", 25);
-            assert.throws(() => scooterApp.loginUser("John", "password456"), Error("Username or password is incorrect"));
-        });
+    it("rents the scooter to a user if it's not broken and has enough charge", () => {
+      const scooter = new Scooter();
+      const user = new User();
+      expect(() => {
+        scooter.rent(user);
+      }).not.toThrow();
+      expect(scooter.user).toBe(user);
     });
+  });
 
-    describe('#logoutUser', function() {
-        it('should log out a logged in user', function() {
-            scooterApp.registerUser("John", "password123", 25);
-            scooterApp.loginUser("John", "password123");
-            scooterApp.logoutUser("John");
-            assert.strictEqual(scooterApp.registeredUsers["John"].loggedIn, false);
-        });
-        it('should throw an error for non-existing user', function() {
-            assert.throws(() => scooterApp.logoutUser("Alice"), Error("no such user is logged in"));
-        });
+  describe("dock", () => {
+    it("updates the station and clears the user", () => {
+      const scooter = new Scooter();
+      const station = "station2";
+      const user = new User();
+      scooter.station = "station1";
+      scooter.user = user;
+      scooter.dock(station);
+      expect(scooter.station).toBe(station);
+      expect(scooter.user).toBeNull();
     });
+  });
 
-    describe('#createScooter', function() {
-        it('should create a new scooter at a station', function() {
-            scooterApp.createScooter("station1");
-            assert.strictEqual(scooterApp.stations["station1"].length, 1);
-        });
-        it('should throw an error for a non-existing station', function() {
-            assert.throws(() => scooterApp.createScooter("station4"), Error("no such station"));
-        });
+  describe("recharge", () => {
+    it("fully charges the scooter", async () => {
+      const scooter = new Scooter();
+      scooter.charge = 50;
+      await scooter.recharge();
+      expect(scooter.charge).toBe(100);
     });
+  });
 
-    describe('#dockScooter', function() {
-        it('should dock a scooter at a station', function() {
-            let scooter = new Scooter();
-            scooterApp.createScooter("station1");
-            scooterApp.dockScooter(scooter, "station1");
-            assert.strictEqual(scooter.station, "station1");
-        });
-        it('should throw an error if scooter is already at station', function() {
-            let scooter = new Scooter();
-            scooter.station = "station1";
-            scooterApp.createScooter("station1");
-            scooterApp.dockScooter(scooter, "station1");
-            assert.throws(() => scooterApp.dockScooter(scooter, "station1"), Error("scooter already at station"));
-        });
- }
+  describe("requestRepair", () => {
+    it("repairs the scooter after 5 seconds", async () => {
+      const scooter = new Scooter();
+      scooter.isBroken = true;
+      await scooter.requestRepair();
+      expect(scooter.isBroken).toBe(false);
+    });
+  });
+});
 
